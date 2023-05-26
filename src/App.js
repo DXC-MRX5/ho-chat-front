1f00b7a6a5541e4c30199ed3ab4c7099056217d0
MXC-DRX5 <deb82nath39@gmail.com>
2023-05-26 10:12:43 +0530

    UI completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import io from "socket.io-client";
+import {useEffect, useState} from "react"
+import { BiSend } from "react-icons/bi";
+
+const socket=io("https://ho-chat-server.onrender.com")
 
 function App() {
+  const [username, setUserName]=useState("")
+  const [msg, setMsg]=useState("")
+  const [messages, setMessages]=useState([])
+
+  useEffect(()=>{
+    socket.on("message",(data)=>{
+      setMessages([...messages, data])
+    })
+  },[messages])
+  const sendMessage=(e)=>{
+    e.preventDefault();
+    socket.emit("chat", {username,msg});
+    setMsg("");
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>React Chat App</h1>
+    <div className='chatWindow'>
+    {messages.map((info, index)=>{
+      return(
+        <div className='chatBox'>
+        {info.username === username ? (
+          <div key={index} className='outgoing'>{info.msg}<span className='name'>You</span></div>
+        ):(
+          <div key={index} className='incoming'>{info.msg}<span className='name'>{info.username}</span></div>
+        )
+        }
+      </div>
+      )
+    })}
+    </div>
+    <div className='typing'>
+        <input type='text' placeholder='Enter Name' value={username} onChange={(e)=>{setUserName(e.target.value)}}/>
+        <textarea type='text' placeholder='Type your message...' value={msg} onChange={(e)=>{setMsg(e.target.value)}} rows='2' cols='25'/>
+        <button type='submit' onClick={sendMessage}><BiSend className='sendIcon'/></button>
+    </div>
     </div>
   );
 }
